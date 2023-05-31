984df2ddfc197224f3d99be450bfb703d464b338
arozovyk <artemiy.rozovyk@ocamlpro.com>
2023-05-31 11:11:42 +0200

    dont write another file

--- src/dune_rules/ocamldep.ml
+++ src/dune_rules/ocamldep.ml
@@ -36,20 +36,6 @@ let parse_module_names ~dir ~(unit : Module.t) ~modules words =
               (Module_name.to_string mname)
           ])
 
-let parse_compilation_units ~modules x y =
-  let loc =
-    let obj_map = Modules.obj_map modules in
-    List.filter_map x ~f:(fun m ->
-        let obj_name = Module_name.Unique.of_string m in
-        match Module_name.Unique.Map.find obj_map obj_name with
-        | Some m -> Some (Module_dep.Local (Modules.Sourced_module.to_module m))
-        | None ->
-          Some (Module_dep.External (Module_dep.External_name.of_string m)))
-  in
-  List.append loc
-    (List.map y ~f:(fun s ->
-         Module_dep.External (Module_dep.External_name.of_string s)))
-
 let parse_deps_exn ~file lines =
   let invalid () =
     User_error.raise
@@ -70,13 +56,24 @@ let parse_deps_exn ~file lines =
       if basename <> Path.basename file then invalid ();
       String.extract_blank_separated_words deps)
 
+let parse_compilation_units ~modules all_deps_output ext =
+  let loc =
+    let obj_map = Modules.obj_map modules in
+    List.filter_map all_deps_output ~f:(fun m ->
+        let obj_name = Module_name.Unique.of_string m in
+        match Module_name.Unique.Map.find obj_map obj_name with
+        | Some m -> Some (Module_dep.Local (Modules.Sourced_module.to_module m))
+        | None ->
+          Some (Module_dep.External (Module_dep.External_name.of_string m)))
+  in
+  List.append loc ext
+
 let deps_of ({ sandbox; modules; sctx; dir; obj_dir; vimpl; stdlib = _ } as md)
     ~ml_kind unit =
   let source = Option.value_exn (Module.source unit ~ml_kind) in
   let dep = Obj_dir.Module.dep obj_dir in
   let context = Super_context.context sctx in
   let all_deps_file = dep (Transitive (unit, ml_kind)) in
-  let ext_deps_file = dep (Ext (unit, ml_kind)) in
   let ocamldep_output = dep (Immediate (unit, ml_kind)) in
   let open Memo.O in
   let transitive_deps modules d =
@@ -110,13 +107,14 @@ let deps_of ({ sandbox; modules; sctx; dir; obj_dir; vimpl; stdlib = _ } as md)
       >>| Action.Full.add_sandbox sandbox)
   in
   let lines = Action_builder.lines_of (Path.build ocamldep_output) in
+  let module_names =
+    Action_builder.map lines ~f:(fun lines ->
+        let parsed = parse_deps_exn ~file:(Module.File.path source) lines in
+        parsed |> parse_module_names ~dir:md.dir ~unit ~modules)
+  in
   let make_paths f filter =
     let open Action_builder.O in
-    let+ lines = lines in
-    let immediate_deps =
-      let parsed = parse_deps_exn ~file:(Module.File.path source) lines in
-      parsed |> parse_module_names ~dir:md.dir ~unit ~modules
-    in
+    let+ immediate_deps = module_names in
     let local =
       List.filter_map immediate_deps ~f:(fun m ->
           (* Maybe have kind in external dep *)
@@ -134,7 +132,6 @@ let deps_of ({ sandbox; modules; sctx; dir; obj_dir; vimpl; stdlib = _ } as md)
     | _ -> false)
     || Option.is_some vimpl
   in
-
   let make_with_file_targets paths file =
     let open Action_builder.O in
     Action_builder.with_file_targets ~file_targets:[ file ]
@@ -152,7 +149,7 @@ let deps_of ({ sandbox; modules; sctx; dir; obj_dir; vimpl; stdlib = _ } as md)
     let+ () = Super_context.add_rule sctx ~dir rule in
     produce_all_deps
   in
-  let* _ =
+  let+ _ =
     let produce_all_deps =
       let paths =
         make_paths
@@ -167,32 +164,33 @@ let deps_of ({ sandbox; modules; sctx; dir; obj_dir; vimpl; stdlib = _ } as md)
     in
     add_rule produce_all_deps
   in
-  let+ _ =
-    if not impl_vmodule then
-      let produce_all_deps_ext =
-        let paths =
-          make_paths
-            (fun (a, b) -> Ext (a, b))
-            (fun m ->
-              match m with
-              | Module_dep.Local _ -> None
-              | External s -> Some (Module_dep.External_name.to_string s))
-        in
-        make_with_file_targets paths ext_deps_file
-      in
-      add_rule produce_all_deps_ext
-    else
-      Memo.return
-        (Action_builder.with_no_targets (Action_builder.return Action.empty))
-  in
+  (* let+ _ =
+       if not impl_vmodule then
+         let produce_all_deps_ext =
+           let paths =
+             make_paths
+               (fun (a, b) -> Ext (a, b))
+               (fun m ->
+                 match m with
+                 | Module_dep.Local _ -> None
+                 | External s -> Some (Module_dep.External_name.to_string s))
+           in
+           make_with_file_targets paths ext_deps_file
+         in
+         add_rule produce_all_deps_ext
+       else
+         Memo.return
+           (Action_builder.with_no_targets (Action_builder.return Action.empty))
+     in *)
   let all_deps_file = Path.build all_deps_file in
-  let ext_deps_file = Path.build ext_deps_file in
+  (*   let ext_deps_file = Path.build ext_deps_file in
+ *)
   let md_l =
     if not impl_vmodule then
       Action_builder.map2
         ~f:(fun x y -> parse_compilation_units ~modules x y)
         (Action_builder.lines_of all_deps_file)
-        (Action_builder.lines_of ext_deps_file)
+        module_names
     else
       Action_builder.map
         ~f:(fun x -> parse_compilation_units ~modules x [])
@@ -202,7 +200,6 @@ let deps_of ({ sandbox; modules; sctx; dir; obj_dir; vimpl; stdlib = _ } as md)
 
 let read_deps_of ~obj_dir ~modules ~ml_kind unit =
   let all_deps_file = Obj_dir.Module.dep obj_dir (Transitive (unit, ml_kind)) in
-
   Action_builder.map
     (Action_builder.lines_of (Path.build all_deps_file))
     ~f:(fun x -> parse_compilation_units ~modules x [])
